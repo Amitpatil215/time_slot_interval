a30cd002a8a44b9578341aac0507bd150f453da0
Amit Patil <amitgpatil215@gmail.com>
2020-10-10 13:07:31 +0530

    Initial Test release

---
+++ lib/provider/date_time_provider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DateTimeProvider with ChangeNotifier {
+   TimeOfDay _selectTime = TimeOfDay(hour: 17, minute: 0);
+  DateTime _selectedDate = DateTime.now();
+  bool _isTelemedication = true;
+
+  TimeOfDay get fetchTime {
+    return _selectTime;
+  }
+
+  DateTime get fetchDate {
+    return _selectedDate;
+  }
+
+  bool get fetchAppointTypeIsTele {
+    return _isTelemedication;
+  }
+
+  DateTime get fetchAppointmentDate {
+    return DateTime(_selectedDate.year, _selectedDate.month, _selectedDate.day,
+        _selectTime.hour, _selectTime.minute);
+  }
+
+  void setTimeSelected(TimeOfDay setTime) {
+    _selectTime = setTime;
+    notifyListeners();
+  }
+
+  void setDateSelected(DateTime setDate) {
+    _selectedDate = setDate;
+    notifyListeners();
+  }
+
+  void setAppoitmentType({bool istele}) {
+    _isTelemedication = istele;
+    notifyListeners();
+  }
+}
